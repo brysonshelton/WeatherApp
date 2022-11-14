f4a079fc897e2c0d8de74b42de212e81f307b3bc
Bryson Shelton <bryson1679@gmail.com>
2022-11-14 17:17:31 -0600

    refactor code

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Shim to load environment variables from .env into ENV in development
+gem "dotenv-rails", groups: [:development, :test]
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-require 'net/http'
-
 class HomeController < ApplicationController
   def index
-    url = "https://api.openweathermap.org/data/2.5/weather?lat=35.2225668&lon=-97.4394777&units=imperial&appid=0367b95e9d1aad34018fe2e844ecfd10"
-    uri = URI(url)
-    res = Net::HTTP.get_response(uri)
-    @data = JSON.parse(res.body)
+    data = CurrentWeatherService.new(latitude: "35.2225668", longitude: "-97.4394777", units: 'imperial').call
+    @weather = Weather.new(data)
   end
 end
